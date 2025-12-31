edd9150818a44106101112fd01cb9263cb00ee08
Florian Rupprecht <floruppr@gmail.com>
2025-12-31 15:47:16 +0100

    vite dows it not work

--- vite.config.js
+++ vite.config.js
@@ -8,7 +8,6 @@ export default defineConfig(({ mode }) => ({
   build: {
     outDir: 'dist',
     sourcemap: false,
-    minify: 'terser',
     rollupOptions: {
       output: {
         manualChunks(id) {
